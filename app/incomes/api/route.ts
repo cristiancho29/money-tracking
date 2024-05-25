4809ab3e1bcc7f1b7df69b2458f244fdd6346d58
cristiancho29 <cristiancho29@gmail.com>
2024-05-25 17:33:44 -0500

    [MT-1] Add GET and POST route handlers for incomes API

--- app/incomes/api/route.ts
+++ app/incomes/api/route.ts
@@ -1,3 +1,33 @@
-export async function GET() {}
+import { sql } from "@vercel/postgres";
 
-export async function POST() {}
+export interface Income {
+  amount: number;
+  description: string;
+  category: string;
+}
+
+function incomeToSQLValues(income: Income) {
+  return `(${income.amount}, ${income.description}, ${income.category})`;
+}
+
+export async function GET() {
+  try {
+    const { rows } = await sql`SELECT * FROM incomes;`;
+    return Response.json({ rows }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  const data: Income = await request.json();
+  try {
+    const result =
+      await sql`INSERT INTO incomes (amount, description, category) VALUES ${incomeToSQLValues(
+        data
+      )};`;
+    return Response.json({ result }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
