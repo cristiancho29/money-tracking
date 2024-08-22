1c3e93d39aec22cfb8bea06c04b5c12ad2050e11
Cristian David Suárez Reina <cristiancho29@gmail.com>
2024-08-21 22:00:39 -0500

    [MT-30] improve navbar look (#20)

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -1,17 +1,47 @@
+import { useStore } from "@nanostores/react";
+import classNames from "classnames";
+import { $currentPath } from "../lib/store/routes";
+
+const routes = [
+  {
+    name: "Dashboard",
+    path: "/",
+  },
+  {
+    name: "Movements",
+    path: "/movements",
+  },
+  {
+    name: "Debts",
+    path: "/debts",
+  },
+];
+
 export default function NavMenu() {
+  const currentPath = useStore($currentPath);
   return (
-    <nav className="w-48 p-4 bg-green-700 h-screen">
-      <h2 className="text-xl font-bold py-2">Money Tracking</h2>
-      <ul>
-        <li className="py-1 text-gray-100 font-semibold">
-          <a href="/">Dashboard</a>
-        </li>
-        <li className="py-1 text-gray-100 font-semibold">
-          <a href="/movements">Movements</a>
-        </li>
-        <li className="py-1 text-gray-100 font-semibold">
-          <a href="/debts">Debts</a>
-        </li>
+    <nav className="w-full bg-green-700">
+      <ul className="flex">
+        {routes.map((route) => (
+          <li
+            key={route.path}
+            className={classNames(
+              "flex flex-1 text-gray-100 font-semibold text-xs",
+              {
+                "bg-green-600": currentPath === route.path,
+              },
+            )}
+          >
+            <a
+              href={route.path}
+              className={classNames("flex w-full justify-center py-4", {
+                "pointer-events-none": currentPath === route.path,
+              })}
+            >
+              {route.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
